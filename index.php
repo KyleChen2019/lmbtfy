ddf49bb45b4de400e03407bfe2cd1552599391dd
aofall <ihacat3004@gmail.com>
2015-10-03 20:09:47 +0800

    Update

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 	<head>
 		<meta name="renderer" content="webkit" charset="utf-8">
-		<meta name="author" content="Retaker">
-		<meta name="keywords" content="let me baidu that for you, 让我帮你百度一下, baidu, retaker,i@retaker.me">
-		<meta name="description" content="有趣的网页,装逼必备,让我帮你百度一下！,调戏小伙伴">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+		<meta name="keywords" content="let me baidu that for you, 让我帮你百度一下">
+		<meta name="description" content="有趣的网页,让我帮你百度一下！,调戏小伙伴">
 		<script src=killie.js></script>
 		<title>让我帮你百度一下</title>
 		<?php if($_GET["q"]==""){ ?>
@@ -14,7 +14,7 @@
 		<script type="text/javascript" src="ZeroClipboard.min.js"></script>
 		<center style="margin-top:50px;">
 			<div id="logo">
-				<span>let me</span><span style="display:none;">baidu</span>
+				<span>Let me</span><span style="display:none;">Baidu</span>
 				<img src="bdlogo.png" id="bdlogo"/>
 				<span>that for you</span>
 			</div>
@@ -24,7 +24,7 @@
 			</div>
 			<div id="instructions">输入你的问题，然后点击 “百度一下”</div>
 			<div id="link">
-				<input id="resultOut" class="link copyable" readonly type="text" value="http://lmbtfy.retaker.me/?q=xxx"/>
+				<input id="resultOut" class="link copyable" readonly type="text" value="./?q=xxx"/>
 				<div id="link_buttons"><a class="link_button" id="copy" href="#" data-clipboard-target="resultOut">复制</a>
 				<a class="link_button" href="#" onclick="location.href=document.getElementById('resultOut').value;">预览</a></div>
 			</div>
@@ -51,10 +51,9 @@
 		<?php } ?>
 		<footer>
 		<div style="display:none;">
-		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253017589'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1253017589%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 		</div>
-		© 2014 retaker.me <a href="http://github.com/retaker/lmbtfy">Github</a><br />
-		本站与百度公司没有任何联系，baidu以及本站出现的百度公司Logo是百度公司的商标。<br />
+		© 2015 让我帮你百度一下 | <a href="https://github.com/aofall/lmbtfy">Github</a> | 作者：<a href="http://lmbtfy.retaker.me" target="_blank" rel="external nofollow">Retaker</a><br/>
+		本站与百度公司没有任何联系，Baidu以及本站出现的百度公司Logo是百度公司的商标。<br/>
 		copyleft, all rights reversed.
 	</footer>
 	</body>
